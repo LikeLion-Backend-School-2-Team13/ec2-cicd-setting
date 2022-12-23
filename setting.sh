ea3c3da250d49d71cb51ce97532e0c74ff8cbed6
menuHwang <hmin0206@naver.com>
2022-12-23 12:10:35 +0900

    권한 및 옵션 추가

--- setting.sh
+++ setting.sh
@@ -1,10 +1,30 @@
-apt-get update
+echo "/////////////////////"
+echo "apt-get update..."
+echo "/////////////////////"
 
-apt-get install \
+sudo apt-get update -y
+
+echo "/////////////////////"
+echo "apt-get update done!
+echo "/////////////////////"
+
+echo "/////////////////////"
+echo "install libraries...
+echo "/////////////////////"
+
+sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release
+    lsb-release -y
+
+echo "/////////////////////"
+echo "install done!
+echo "/////////////////////"
+
+echo "////////////////////"
+echo "registry key"
+echo "////////////////////"
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -12,3 +32,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
